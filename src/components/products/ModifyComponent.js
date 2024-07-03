bae0b0b35ba43ea0a709181ffddb2f228945d291
ssooya90 <ssooya90@naver.com>
2024-07-04 00:05:55 +0900

    상품 수정까지

--- src/components/products/ModifyComponent.js
+++ src/components/products/ModifyComponent.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
 import FetchingModal from "../common/FetchingModal";
-import {getOne} from "../../api/productsApi";
+import {deleteOne, getOne, putOne} from "../../api/productsApi";
 import {API_SERVER_HOST} from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
+import ResultModal from "../common/ResultModal";
 
 const initState = {
 	pno: 0,
@@ -17,26 +19,23 @@ function ModifyComponent({pno}) {
 	const host = API_SERVER_HOST
 
 	const [product, setProduct] = useState(initState)
-	const [fetching, setFetching] = useState()
+	const [fetching, setFetching] = useState(false)
+	const [result, setResult] = useState(false)
+
+	const {moveToList, moveToRead} = useCustomMove()
 
 	useEffect(() => {
 
 		setFetching(true)
 
 		getOne(pno).then( data => {
-
 			console.log(data)
-
 			setProduct(data)
-
 			setFetching(false)
-
-
 		})
 
 	}, [initState]);
 
-	const uploadRef = useRef();
 
 	const handleChangeProduct = (e) => {
 
@@ -53,6 +52,53 @@ function ModifyComponent({pno}) {
 
 	}
 
+	const uploadRef = useRef();
+
+	const handleClickModify = () => {
+		const files = uploadRef.current.files
+		const formData = new FormData();
+
+		for(let i = 0 ; i < files.length ; i++){
+			formData.append('files',files[i]);
+		}
+
+		formData.append("pname", product.pname)
+		formData.append("pdesc", product.pdesc)
+		formData.append("price", product.price)
+
+		for(let i = 0 ; i <product.uploadFileNames.length; i++){
+			formData.append('uploadFileNames',product.uploadFileNames[i]);
+		}
+
+		setFetching(true);
+
+		putOne(pno, formData).then(data => {
+
+			setResult('Modified');
+			setFetching(false);
+		})
+	}
+
+	const handleClickDelete = () => {
+		setFetching(true)
+
+		deleteOne(pno).then(data => {
+			setResult('Deleted')
+			setFetching(false);
+		})
+	}
+
+	const closeModal = () => {
+
+		if(result === 'Modified'){
+			moveToRead(pno)
+		}else if(result === 'Deleted'){
+			moveToList({page:1})
+		}
+
+		setResult(null);
+	}
+
 	const deleteOldImages = (imageName) => {
 
 		const resultFileNames = product.uploadFileNames.filter(fileName => fileName !== imageName)
@@ -66,6 +112,13 @@ function ModifyComponent({pno}) {
 	return (
 			<div className="border-2 border-sky-200 mt-10 m-2 p-4">
 				{fetching ? <FetchingModal/> : <></>}
+
+				{result ? <ResultModal
+						title={`${result}`}
+						content={'처리되었습니다'}
+						callbackFn={closeModal}
+				/> : <></>}
+
 				<div className="flex justify-center">
 					<div className="relative mb-4 flex w-full flex-wrap items-stretch">
 						<div className="w-1/5 p-6 text-right font-bold">Product Name</div>
@@ -112,13 +165,28 @@ function ModifyComponent({pno}) {
 						<div className="w-4/5 justify-center flex flex-wrap items-start">
 							{product.uploadFileNames.map((imgFile, i) =>
 									<div className="flex justify-center flex-col w-1/3 m-1 align-baseline" key={i}>
-										<button className="bg-blue-500 text-3xl text-white" onClick={() => deleteOldImages(imgFile)}>DELETE</button>
+										<button className="bg-blue-500 text-3xl text-white"
+										        onClick={() => deleteOldImages(imgFile)}>DELETE
+										</button>
 										<img alt="img" src={`${host}/api/products/view/s_${imgFile}`}/>
 									</div>
 							)}
 						</div>
 					</div>
 				</div>
+
+				<div className="flex justify-end p-4">
+					<button type="button" onClick={handleClickDelete} className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500">
+						Delete
+					</button>
+					<button type="button" onClick={handleClickModify} className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-orange-500">
+						Modify
+					</button>
+					<button type="button" onClick={() => moveToList()} className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500">
+						List
+					</button>
+				</div>
+
 			</div>
 
 	);
