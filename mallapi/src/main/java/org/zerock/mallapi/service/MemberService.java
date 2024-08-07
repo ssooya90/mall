60921f228680e19c9dd3636d38c22a17beb48229
ssooya90 <ssooya90@naver.com>
2024-08-07 23:56:09 +0900

    카카오 로그인 연동

--- mallapi/src/main/java/org/zerock/mallapi/service/MemberService.java
+++ mallapi/src/main/java/org/zerock/mallapi/service/MemberService.java
@@ -1,7 +1,10 @@
 package org.zerock.mallapi.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mallapi.domain.Member;
 import org.zerock.mallapi.dto.MemberDTO;
+
+import java.util.stream.Collectors;
 
 @Transactional
 public interface MemberService {
@@ -9,6 +12,18 @@ public interface MemberService {
 	MemberDTO getKakaoMember(String accessToken);
 
 
+	default MemberDTO entityToDTO(Member member) {
+
+		MemberDTO dto = new MemberDTO(
+				member.getEmail(),
+				member.getPw(),
+				member.getNickname(),
+				member.isSocial(),
+				member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList()));
+
+				return dto;
+
+	}
 
 
 
--- mallapi/src/main/java/org/zerock/mallapi/service/MemberServiceImpl.java
+++ mallapi/src/main/java/org/zerock/mallapi/service/MemberServiceImpl.java
@@ -6,14 +6,18 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.zerock.mallapi.domain.Member;
+import org.zerock.mallapi.domain.MemberRole;
 import org.zerock.mallapi.dto.MemberDTO;
 import org.zerock.mallapi.repository.MemberRepository;
 
 import java.util.LinkedHashMap;
+import java.util.Optional;
 
 
 @Service
@@ -23,22 +27,61 @@ public class MemberServiceImpl implements MemberService{
 
 	private final MemberRepository memberRepository;
 
+	private final PasswordEncoder passwordEncoder;
+
+
+
+
 	@Override
 	public MemberDTO getKakaoMember(String accessToken) {
 
-		getEmailFromKakaoAcessToken(accessToken);
+		String email = getEmailFromKakaoAcessToken(accessToken);
 
 		//accessToken을 이용해서 사용자 정보 가져오기
 
 		// 기존에 DB에 회원 정보가 있는 경우 / 없는 경우
 
+		Optional<Member> result = memberRepository.findById(email);
 
+		if(result.isPresent()){
 
-		return null;
+			MemberDTO memberDTO = entityToDTO(result.get());
+
+			log.info("existed....." + memberDTO);
+
+			 return memberDTO;
+		}
+
+		Member socialMember = makeSocialMember(email);
+
+		memberRepository.save(socialMember);
+
+		MemberDTO memberDTO = entityToDTO(socialMember);
+
+		return memberDTO;
+	}
+
+	private Member makeSocialMember(String email){
+
+		String tempPassword = makeTempPassword();
+
+		log.info("tempPassword: " + tempPassword);
+
+		Member member = Member.builder()
+				.email(email)
+				.pw(passwordEncoder.encode(tempPassword))
+				.nickname("social Member")
+				.social(true)
+				.build();
+
+		member.addRole(MemberRole.USER);
+
+		return member;
+
 	}
 
 
-	private void getEmailFromKakaoAcessToken(String accessToken) {
+	private String getEmailFromKakaoAcessToken(String accessToken) {
 
 		String kakaoGetUser = "https://kapi.kakao.com/v2/user/me";
 
@@ -56,14 +99,34 @@ public class MemberServiceImpl implements MemberService{
 				restTemplate.exchange(uriComponents.toUri(), HttpMethod.GET, entity, LinkedHashMap.class);
 
 
+		log.info("response.......................");
 		log.info(response);
 
 
 		LinkedHashMap<String, LinkedHashMap> bodyMap = response.getBody();
+		LinkedHashMap<String, LinkedHashMap> kakaoAccount = bodyMap.get("kakao_account");
 
-		log.info("===========");
+
 		log.info(bodyMap);
+		log.info("kakaoAccount " + kakaoAccount.get("email"));
+
+		String email = String.valueOf(kakaoAccount.get("email"));
+
+		return email;
 
 
+
+	}
+
+
+	private String makeTempPassword() {
+
+		StringBuffer buffer = new StringBuffer();
+
+		for(int i = 0; i< 10 ; i++){
+			buffer.append((char)((int)(Math.random()*55) + 65));
+		}
+
+		return buffer.toString();
 	}
 }
