d8d3fbfabe38e800598275b53a0acd3dc7b29a9a
ssooya90 <ssooya90@naver.com>
2024-08-16 23:38:52 +0900

    장바구니 테이블 설계

---
+++ mallapi/src/main/java/org/zerock/mallapi/domain/Cart.java
@@ -0,0 +1,28 @@
+package org.zerock.mallapi.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "owner")
+@Table(
+		name ="tbl_cart",
+		indexes = { @Index(name="idx_cart_email", columnList = "member_owner")}
+)
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cno;
+
+	@OneToOne
+	@JoinColumn(name="member_owner") // 컬럼명을 사용하는 이유는 인덱스를 쓰기 위해서
+	private Member member;
+
+}
